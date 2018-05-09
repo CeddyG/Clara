8b2f17264b6c67ae6bc8b6c448e0f996c4e0a3a8
CeddyG <ceddyg@free.fr>
2018-05-09 23:47:38 +0200

    -Set throttle before starting session in API middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,10 +38,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
         ],
     ];
 
