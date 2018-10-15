d2d72a39b3c62450c407f693b534a72c8795ee52
CeddyG <ceddyg@free.fr>
2018-10-15 17:01:08 +0200

    -Add general translation

---
+++ resources/lang/en/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the form. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'yes'       => 'Yes',
+    'no'        => 'No',
+    'sign-out'  => 'Logout',
+    'close'     => 'Close',
+    'save'      => 'Save',
+    'send'      => 'Send',
+    'return'    => 'Return'
+];
