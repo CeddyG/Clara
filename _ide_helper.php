153bb30c735a3e2039e401334805b4e3f2034d08
CeddyG <ceddyg@free.fr>
2019-12-24 16:45:45 +0100

    -Some fixes

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.9.0 on 2019-12-24 15:02:12.
+ * Generated for Laravel 6.9.0 on 2019-12-24 15:45:15.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- config/clara.navbar.php
+++ config/clara.navbar.php
@@ -2,5 +2,4 @@
 
 return [
     
-    'article' => 'Article',
 ];
