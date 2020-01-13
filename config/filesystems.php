52de708a79a569b5165e0de18e06041b96a9bedf
CeddyG <ceddyg@free.fr>
2020-01-13 16:07:44 +0100

    -Add base driver to filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'base' => [
+            'driver' => 'local',
+            'root' => base_path()
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
